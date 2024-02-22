correct coordinate transformation in epl potential; wrong sign issue still present?

compote/potentials/epl.py
@@ -10,7 +10,6 @@ class EPL:
     def potential(self, phi, theta, f, theta_E, gamma):
         """
         EPL potential from Tessore and Metcalf 2015 (https://arxiv.org/abs/1507.01819), eqn 14 with coordinate transformation given in eqn 5
-        Their R is my theta and their \varphi is my phi.
         :param phi: angular coordinate
         :param theta: radial coordinate
         :param f: axis ratio (between 0 and 1)
@@ -21,7 +20,11 @@ class EPL:
         b = theta_E*np.sqrt(f)
         t = gamma-1
         prefactor = 1/(2-t)
-        alpha = ((2*b)/(1+f))*((b/theta)**(t-1))*(np.exp(1j*phi))*(hyp2f1(1, t/2, 2-(t/2), -((1-f)/(1+f))*np.exp(1j*2*phi))) # eqn 14
-        z_coordinate = (theta/f)*np.cos(phi) + 1j*theta*np.sin(phi)
+        theta_1 = theta*np.cos(phi)
+        theta_2 = theta*np.sin(phi)
+        R = np.sqrt((f**2.)*(theta_1**2.) + theta_2**2.)
+        phi_ell = np.arctan2(theta_2, f*theta_1)
+        alpha = ((2*b)/(1+f))*((b/R)**(t-1))*(np.exp(1j*phi_ell))*(hyp2f1(1, t/2, 2-(t/2), -((1-f)/(1+f))*np.exp(1j*2*phi_ell))) # eqn 14
+        z_coordinate = (R/f)*np.cos(phi_ell) + 1j*R*np.sin(phi_ell)
         potential = prefactor*(((z_coordinate*np.conjugate(alpha)) + (np.conjugate(z_coordinate)*alpha))/2)
         return potential