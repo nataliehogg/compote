f2b6c9d36218a4a60970c451e72511150515f139
nataliehogg <mail@nataliebhogg.com>
2024-02-23 10:45:57 +0100

    correct arctan argument ordering

--- compote/potentials/epl.py
+++ compote/potentials/epl.py
@@ -22,8 +22,8 @@ class EPL:
         prefactor = 1/(2-t)
         theta_1 = theta*np.cos(phi)
         theta_2 = theta*np.sin(phi)
-        R = np.sqrt(theta_1**2. + (f**2.)*(theta_2**2.))
-        phi_ell = np.arctan2(f*theta_2, theta_1)
+        R = np.sqrt(theta_1**2. + f**2.*theta_2**2.)
+        phi_ell = np.arctan2(theta_1, f*theta_2)
         alpha = ((2*b)/(1+f))*((b/R)**(t-1))*(np.exp(1j*phi_ell))*(hyp2f1(1, t/2, 2-(t/2), -((1-f)/(1+f))*np.exp(1j*2*phi_ell))) # eqn 14
         z_coordinate = (R/f)*np.cos(phi_ell) + 1j*R*np.sin(phi_ell)
         potential = prefactor*(((z_coordinate*np.conjugate(alpha)) + (np.conjugate(z_coordinate)*alpha))/2)
